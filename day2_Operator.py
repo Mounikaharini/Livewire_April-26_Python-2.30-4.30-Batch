98bcbcc11056abdc27a197e29ea21f46ff892465
Mounika Harini <146058701+Mounikaharini@users.noreply.github.com>
2026-04-13 16:19:53 +0530

    Create day2_Operator.py

---
+++ day2_Operator.py
@@ -0,0 +1,49 @@
+'''
+#comparision operator
+==
+!=
+<
+>
+<=
+>=
+
+a = 10
+b = 20
+print(a==b)
+print(10==10)
+print(a!=b)
+print(10!=10)
+print(10>5)
+print(5<40)
+print(10<10)
+print(10>10)
+print(10<=10)
+print(100>=40)
+
+age=18
+print(age>18 or age==18)
+print(10>=10 and 10<=10)
+print(not age>=18)
+'''
+#membership
+a = [1,2,3,4,5]
+print(3 in a)
+print(6 in a)
+
+print(6 not in a)
+print(3 not in a)
+
+#identity
+x = 10
+y = 10
+print(x is y)
+x1 = 10
+x2 = 10
+print(x1 is not x2)
+
+
+
+
+
+
+
