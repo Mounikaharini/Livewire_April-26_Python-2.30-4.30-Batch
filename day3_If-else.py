7101742e5e7bfb82e13d8acdb23c0d2d24527cd6
Mounika Harini <146058701+Mounikaharini@users.noreply.github.com>
2026-04-13 16:20:48 +0530

    Create day3_If-else.py

---
+++ day3_If-else.py
@@ -0,0 +1,48 @@
+'''
+print("Start")
+
+age = 18
+if age>=18:
+    print("Eligible to vote")
+
+print("End")
+
+print("Start")
+
+age = 8
+if age>=18:
+    print("Eligible to vote")
+
+print("End")
+'''
+
+'''
+x = 10
+if x==10:
+    print("hi")
+else:
+    print("condition false")
+
+
+
+y = 10
+if y%2==0:
+    # y%2 == 0 ->10%2=0 -> 0 == 0 ->true
+    print("Even")
+else:
+    print("Odd")
+
+mark = int(input("Enter the mark:"))
+
+if mark>=35:
+    print("Pass")
+else:
+    print("Fail")
+'''
+
+
+
+
+
+
+
