bcd2e71e58e4503cce13be9091884c2919dc0b83
Mounika Harini <146058701+Mounikaharini@users.noreply.github.com>
2026-04-17 16:34:43 +0530

    Create day4_forLoop.py

---
+++ day4_forLoop.py
@@ -0,0 +1,30 @@
+#bitwise
+print(92&190)
+print(92|190)
+print(92^190)
+print(~5)
+print(~(-5))
+print(92<<1)
+print(92>>1)
+
+for i in range(1,6,1):
+    print(i)
+
+for i in range(5,0,-1):
+    print(i)
+    
+# 1 2 3 4 5
+for i in range(1,6,1):
+    print(i,end=" ")
+
+# * * * * *
+for i in range(1,6,1):
+    print("*",end=" ")
+
+#task1 - 3 6 9 12 15
+#task2 - 90 70 50 30 10
+
+for i in range(90,9,-20):
+    print(i,end=" ")
+
+
